2afff742e1211806caa81927866ca551660e632c
William Bigas Mauro <william.mauro@ibm.com>
2024-02-21 16:48:25 -0300

    Initial commit

---
+++ src/main/java/br/com/willbigas/Application.java
@@ -0,0 +1,13 @@
+package br.com.willbigas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteComTestcontainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteComTestcontainersApplication.class, args);
+	}
+
+}
